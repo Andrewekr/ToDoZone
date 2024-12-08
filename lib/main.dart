d0b781710c932aee30fd26b8d80c448cd2f7fed7
Andrew Rubtsou <39550945+Andrewekr@users.noreply.github.com>
2024-12-08 19:06:36 +0300

    epdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
-import 'package:flutter_application_1/view_models/app_view_model.dart';
-import 'package:provider/provider.dart';
-
-import 'views/task_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/view_models/app_view_model.dart';
+import 'views/task_page.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (context) => appViewModel(), child: const MyApp()));
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppViewModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: TaskPage(),
-    ); // MaterialApp
+    );
   }
 }
